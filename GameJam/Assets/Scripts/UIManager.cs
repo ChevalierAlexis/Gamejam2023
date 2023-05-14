d8289eefe7e70cb90e58fef519d37e9e0ff070cb
Alexis <al.chevalier@rns.ecolescreatives.com>
2023-05-14 14:33:41 +0200

    Menu amélioration

--- GameJam/Assets/Scripts/UIManager.cs
+++ GameJam/Assets/Scripts/UIManager.cs
@@ -8,14 +8,23 @@ public class UIManager : MonoBehaviour
 {
     private Player PlayerRef;
     public TMP_Text PVText;
+    public Manager ManaRef;
+    public TMP_Text WaveText;
+    public TMP_Text MaxWaveText;
+    public GameObject AmeliorationMenu;
+
     void Start()
     {
         var listplayer = FindObjectsOfType<Player>();
         PlayerRef = listplayer[0];
+        AmeliorationMenu.SetActive(false);
     }
 
     void Update()
     {
         PVText.text = PlayerRef.PV.ToString();
+        WaveText.text = ManaRef.CurrentWave.ToString();
+        MaxWaveText.text = ManaRef.MaxWave.ToString();
+        
     }
 }
